929fc6ff75df27b7d13accbe736dbd31bd2fc49f
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-08-24 20:59:33 +0200

    debuged chat code

--- components/Chat.js
+++ components/Chat.js
@@ -1,105 +1,98 @@
 import React from "react";
 import { View, Platform, Button, KeyboardAvoidingView } from "react-native";
+//import AsyncStorage from "@react-native-community/async-storage";
 
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
-import firebase from "firebase";
+const firebase = require("firebase");
+require("firebase/firestore");
 
-//Firebase DB
-//const firebase = require('firebase');
-//require('firebase/firestore');
+const firebaseConfig = {
+  apiKey: "AIzaSyASaZho3f5xkW6LF6415NtoUZwGSBLNKKg",
+  authDomain: "chat-app-cedf3.firebaseapp.com",
+  projectId: "chat-app-cedf3",
+  storageBucket: "chat-app-cedf3.appspot.com",
+  messagingSenderId: "416532966096",
+  appId: "1:416532966096:web:8070b549943c9454e70cd5",
+  measurementId: "G-VJEPJLKFD7"
+};
+
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
-
-    // Firebase config details
-   const firebaseConfig = {
-    apiKey: "AIzaSyASaZho3f5xkW6LF6415NtoUZwGSBLNKKg",
-    authDomain: "chat-app-cedf3.firebaseapp.com",
-    projectId: "chat-app-cedf3",
-    storageBucket: "chat-app-cedf3.appspot.com",
-    messagingSenderId: "416532966096",
-    appId: "1:416532966096:web:8070b549943c9454e70cd5",
-    measurementId: "G-VJEPJLKFD7"
-   }
-   if (!firebase.apps.length) {
-     firebase.intitializeApp(firebaseConfig);
-   }
-
-   this.referenceChatMessages = firebase.firestore().collection('messages');
-   this.referenceMessageUser = null;
-
-    this.state={
+    this.state = {
       messages: [],
       uid: 0,
+      loggedInText: "Logging in...",
       user: {
-        _id: '',
-        name: '',
+        _id: "",
+        name: "",
       },
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+    this.referenceMessageUser = null;
+  }
+
+  async getMessages() {
+    let messages = "";
+    try {
+      messages = (await AsyncStorage.getItem("messages")) || [];
+      this.setState({
+        messages: JSON.parse(messages),
+      });
+    } catch (error) {
+      console.log(error.message);
     }
   }
 
-  componentDidMount() {
-    let username = this.props.route.params.username;
-    this.props.navigation.setOptions({ title: username });
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (!user) {
-        firebase.auth().signInAnonymously();
-      }
-    },
-    this.setState({
-      uid:user.uid,
-      messages: [
-        {
-          _id:1,
-          createdAt: new Date(),
-          text: 'Hello Developer',
-          user: {
-            _id:4,
-            name: 'React Feedback',
-            avatar: 'https://placeimg.com/140/140/any',
-          }
-        },
-        {
-          _id:2,
-          text: 'Welcome to the "talk" Chat App, it is ' + new Date() + ' Welcome! For additional help visit the docs at https://#.github.io/talk ',
-          createdAt: new Date(),
-          user: {
-            _id:3,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 3,
-          text:'This is a system message',
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
-    }),
-    this.unsubscribe = this.referenceChatMessages
-      .orderBy("createdAt", "desc")
-      .onSnapshot(this.onCollectionUpdate));
+  async saveMessage() {
+    try {
+      await AsyncStorage.setItem(
+        "messages",
+        JSON.stringyfy(this.state.messages)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
   }
-  componentWillUnmount() {
-    this.authUnsubscribe();
+
+  async deleteMessages() {
+    try {
+      await AsyncStorage.removeItem("messages");
+      this.setState({
+        messages: [],
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  addMessage() {
+    const message = this.state.messages[0];
+    // add the new messages to the collection
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
   }
 
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-  }
-
-  //add messages to database
-  addMessages() {
-    this.referenceChatMessages.addMessages({
-      _id: messages._id,
-      createdAt: messages.createdAt,
-      text: messages.text,
-      user: messages.user,
-    })
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.saveMessage();
+      }
+    );
   }
 
   onCollectionUpdate = (querySnapshot) => {
@@ -112,55 +105,86 @@ export default class Chat extends React.Component {
         _id: data._id,
         text: data.text,
         createdAt: data.createdAt.toDate(),
-        user: data.user,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        },
       });
     });
     this.setState({
       messages,
     });
   };
+
   renderBubble(props) {
     return (
       <Bubble
         {...props}
-          wrapperStyle={{
-            right: {
-              backgroundColor: "#000"
-            }
-          }}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+            // Background color for my own chat window. Could be adjusted to fit the color concept to a greenish tone
+          },
+        }}
       />
     );
   }
 
+  componentDidMount() {
+    const name = this.props.route.params.username;
+    this.props.navigation.setOptions({ title: name });
+
+    this.getMessages();
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+        },
+      });
+      this.referenceMessagesUser = firebase
+        .firestore()
+        .collection("messages")
+        .where("uid", "==", this.state.uid);
+
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  componentWillUnmount() {
+    // stop authentication
+    this.authUnsubscribe();
+  }
+
   render() {
     return (
-      // <View
-      //   style={{
-      //     flex: 1,
-      //     justifyContent: "center",
-      //     alignItems: "center",
-      //     backgroundColor: this.props.route.params.backgroundColor,
-      //   }}>
-      //   <Button
-      //     title='Go to Start'
-      //     onPress={() => this.props.navigation.navigate("Start")}
-        // />
-        <View  style={{
+      <View
+        style={{
           flex: 1,
-          // justifyContent: "center",
-          // alignItems: "center",
+
           backgroundColor: this.props.route.params.backgroundColor,
-        }}>
-          <GiftedChat 
-            renderBubble={this.renderBubble.bind(this)}
-            messages={this.state.messages}
-            onSend={messages => this.onSend(messages)}
-            user={{
-              _id:1,
-            }}
-          />
-          {Platform.OS === 'android' ? <KeyboardAvoidingView behaviour='height' /> : null }
-        </View>
+        }}
+      >
+        <GiftedChat
+          messages={this.state.messages}
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={this.state.user}
+        />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behaviour="height" />
+        ) : null}
+        {/* This part is important for Issues with Android Keyboard covering Chat window */}
+      </View>
       // </View>
     );
   }
